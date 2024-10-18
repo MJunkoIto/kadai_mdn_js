665846d82d0a832cb6d6eada8c5ff103156602de
MJunkoIto <soutozen2018@gmail.com>
2024-10-18 22:32:31 +0900

    Add files via upload

--- mdn.js
+++ mdn.js
@@ -7,6 +7,6 @@ const month = String(today.getMonth() + 1).padStart(2, '0'); // 月は0から始
 const day = String(today.getDate()).padStart(2, '0');        // 2桁表示にパディング
 
 // 希望の形式で日付を表示
-const formattedDate = `${2022年}/${8月}/${22日}`;
+const formattedDate = 2024 + "年" + 10 + "月" + 18 + "日";
 
 console.log(formattedDate);
